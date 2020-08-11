8775b5f3663664074fe4a638c7e1e5b33b8afdd1
GitHub Action <action@github.com>
2020-08-11 10:40:02 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/alexhandbybbc/trbintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.alexhandbybbc.trbintellijplugin.services
+
+import com.github.alexhandbybbc.trbintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
